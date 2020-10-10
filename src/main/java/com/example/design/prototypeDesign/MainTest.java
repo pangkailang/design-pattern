14f183781c00e1038ed6e5ed921504e1572aba52
pangkailang <330891873@qq.com>
2020-10-11 00:30:59 +0800

    原型模式

---
+++ src/main/java/com/example/design/prototypeDesign/MainTest.java
@@ -0,0 +1,26 @@
+package com.example.design.prototypeDesign;
+
+public class MainTest {
+    public static void main(String[] args) throws Exception{
+        User user = new User("张三",20);
+        Dept dept = new Dept("销售部", "007");
+        //使用浅拷贝
+        User user1=(User) user.clone();
+        System.out.println(user1.getAge());
+        System.out.println(user==user1);
+        System.out.println("..................");
+        user.setDept(dept);
+        //使用浅拷贝
+        User user2=(User)user.clone();
+        System.out.println(user2.getAge());
+        System.out.println(user==user2);
+        System.out.println(user.getDept()==user2.getDept());
+        System.out.println("..................");
+        //使用深拷贝
+        User user3=(User)user.deepClone();
+        System.out.println(user3.getAge());
+        System.out.println(user==user3);
+        System.out.println(user.getDept()==user3.getDept());
+        System.out.println("..................");
+    }
+}
---
+++ src/main/java/com/example/design/prototypeDesign/User.java
@@ -0,0 +1,35 @@
+package com.example.design.prototypeDesign;
+
+import lombok.Data;
+
+import java.io.*;
+
+@Data
+public class User implements Cloneable,Serializable {
+    private String name;
+    private Integer age;
+    private Dept dept;
+
+    public User(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+    //浅拷贝
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return (User)super.clone();
+    }
+    //深拷贝
+    public Object deepClone() throws IOException, ClassNotFoundException {
+
+        /* 写入当前对象的二进制流 */
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(bos);
+        oos.writeObject(this);
+
+        /* 读出二进制流产生的新对象 */
+        ByteArrayInputStream bis = new ByteArrayInputStream(bos.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bis);
+        return ois.readObject();
+    }
+}
